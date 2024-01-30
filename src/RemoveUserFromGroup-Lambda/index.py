9c6696bcd81c3bd7863e963714377290af7d3582
ravi.rana <ravi.rana@tothenew.com>
2024-01-30 17:28:15 +0530

    Updated: Revoke Lambda

--- src/RemoveUserFromGroup-Lambda/index.py
+++ src/RemoveUserFromGroup-Lambda/index.py
@@ -1,9 +1,23 @@
 import boto3
 import os
 
-identityClient = boto3.client('identitystore')
-groupTable = os.getenv('GROUPTABLE')
+stsClient = boto3.client('sts')
+
+metadataTable = os.getenv('METADATATABLE')
 identityStore = os.getenv('IDENTITYSTOREID')
+crossAccRole = os.getenv('CROSSACCROLE')
+
+crossAcc = stsClient.assume_role(
+    RoleArn = crossAccRole,
+    RoleSessionName = "CKAMRevokePermission"
+)
+ACCESSKEY = crossAcc['Credentials']['AccessKeyId']
+SECRETKEY = crossAcc['Credentials']['SecretAccessKey']
+SESSIONTOKEN = crossAcc['Credentials']['SessionToken']
+
+identityClient = boto3.client('identitystore', aws_access_key_id=ACCESSKEY, aws_secret_access_key=SECRETKEY, aws_session_token=SESSIONTOKEN)
+
+
 
 def removeFromGroup(membershipId):
     try:
@@ -15,5 +29,5 @@ def removeFromGroup(membershipId):
         print('Not able to remove permission' + str(e))
 
 def lambda_handler(event, context):
-    membershipId = event['MembershipId']
+    membershipId = event['grantoutput']['Payload']['MembershipId']
     removeFromGroup(membershipId)
