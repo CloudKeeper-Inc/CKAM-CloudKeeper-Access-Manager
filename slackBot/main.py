facf19fc6832b87dba80154963ec753236121d21
JatinTTN <jatin.rautela@tothenew.com>
2025-02-08 01:25:29 +0530

    update: env variable for dynamodb region

--- slackBot/main.py
+++ slackBot/main.py
@@ -24,7 +24,7 @@ app = App(
 requestTable = os.getenv('REQUESTTABLENAME')
 approverTable = os.getenv('APPROVERTABLENAME')
 accessManagerTable = os.getenv('ACCESSMANAGERMETADATATABLE')
-dynamoClient = boto3.client('dynamodb')
+dynamoClient = boto3.client('dynamodb', region_name=os.getenv('DYNAMODBREGION'))
 
 
 @app.event("app_mention")
